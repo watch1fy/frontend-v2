903d70410cbcd55e1fbd6a060169744f23d6f0e3
Anmol Sudhir <anmolsudhir.2001@gmail.com>
2024-02-20 15:41:59 +0530

    feat(landing): Added features section

---
+++ config/features.ts
@@ -0,0 +1,29 @@
+const features = [
+  {
+    heading: 'Real-time Synchronized Watching',
+    content: "Experience seamless movie nights with friends or loved ones, regardless of your physical location. With our synchronized watching feature, you'll feel like you're sitting next to each other, sharing every moment in real-time.",
+    src: '/illustrations/together.svg'
+  },
+  {
+    heading: 'Interactive Text and Video Chat',
+    content: "Connect face-to-face while watching your favorite content together. Our integrated video chat feature lets you see each other's reactions, laugh together, and make every viewing session feel like a shared experience.",
+    src: '/illustrations/chatnvid.svg'
+  },
+  {
+    heading: 'Dual Control Playback',
+    content: "Take turns controlling playback, ensuring both you and your partner have a say in what happens next. Whether it's pausing for a bathroom break or rewinding to catch a crucial scene, our dual control system keeps things fair and interactive.",
+    src: '/illustrations/dual.svg'
+  },
+  {
+    heading: 'User-Uploaded Content with Privacy Options',
+    content: "Share your own videos with ease, whether they're personal memories or creative projects. With customizable privacy settings, you can choose who gets to see your content, keeping it private or sharing it with the community as you see fit.",
+    src: '/illustrations/upload.svg'
+  },
+  {
+    heading: 'Live Presence Indicators and Emoji Reactions',
+    content: "Stay connected with your partner through real-time presence indicators, showing their activities like mouse movements. Express instant reactions with emojis, adding a layer of fun and interactivity to your watching sessions and chats.",
+    src: '/illustrations/test.svg'
+  },
+]
+
+export default features;
