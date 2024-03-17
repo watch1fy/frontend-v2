ffb8ac5c0f8d6909eaeac6373796cf32a3bd3d94
Anmol Sudhir <anmolsudhir.2001@gmail.com>
2024-03-17 12:13:06 +0530

    feat: Added movie and tv fetching functions

--- lib/functions/tv/index.ts
+++ lib/functions/tv/index.ts
@@ -1,19 +1,3 @@
-export const getPopularMovies = async () => {
-  let movies_res: any;
-  try {
-    movies_res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&sort_by=popularity.desc`,
-      {
-        cache: "force-cache",
-      },
-    ).then((data) => data.json());
-  } catch {}
-
-  const movies: any[] = movies_res?.results;
-
-  return movies;
-};
-
 export const getPopularTv = async () => {
   let movies_res: any;
   try {
@@ -23,23 +7,23 @@ export const getPopularTv = async () => {
         cache: "force-cache",
       },
     ).then((data) => data.json());
-  } catch {}
+  } catch { }
 
   const movies: any[] = movies_res?.results;
 
   return movies;
 };
 
-export const getAllTrending = async () => {
+export const getTopRatedTv = async () => {
   let movies_res: any;
   try {
     movies_res = await fetch(
-      `https://api.themoviedb.org/3/trending/all/week?language=en-US&api_key=${process.env.TMDB_API_KEY}`,
+      `https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=vote_average.desc&vote_count.gte=200&api_key=${process.env.TMDB_API_KEY}`,
       {
         cache: "force-cache",
       },
     ).then((data) => data.json());
-  } catch {}
+  } catch { }
 
   const movies: any[] = movies_res?.results;
 
