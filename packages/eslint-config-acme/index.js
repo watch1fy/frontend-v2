4ab4a4f322d87d150f764532383e0759f7b81fb0
Anmol Sudhir <anmolsudhir.2001@gmail.com>
2024-06-04 16:26:05 +0530

    init(micro-frontend): Initialized micro-frontend

--- packages/eslint-config-acme/index.js
+++ packages/eslint-config-acme/index.js
@@ -1,6 +1,17 @@
 module.exports = {
   extends: ['next', 'turbo', 'prettier'],
+  plugins: ['unused-imports'],
   rules: {
-    '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 }
